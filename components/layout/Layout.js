ac9c85cabf8d28976db03eef25df670e40c9bd6f
Tan Vo <tan.vopm@gmail.com>
2023-01-26 15:26:48 +0000

    check meta data

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,16 +2,32 @@ import Head from "next/head"
 import { CourseHeader, MainHeader } from "./Header"
 
 import Footer from "./Footer"
+import { useEffect, useState } from "react"
 
 
-
+const metaDecorator = {
+    hostname: "https://medlangfanatic.com"
+}
 
 export default function Layout({ title, description, children }) {
-    return (
+    const [loaded, setLoaded] = useState(false)
+    useEffect(() => {
+        setLoaded(true)
+    }, [])
+    return loaded && (
         <div>
             <Head>
                 <title>{title}</title>
                 <meta name="description" content={description} />
+                <meta property="og:type" content="website" />
+                <link rel='canonical' href={metaDecorator.hostname + window.location.pathname + window.location.search} />
+                <meta
+                    property="og:url"
+                    content={metaDecorator.hostname + window.location.pathname + window.location.search}
+                />
+                <meta property="og:title" content={title} />
+                <meta property="og:image" content={"https://res.cloudinary.com/tanvo/image/upload/v1674322596/medlangfanatic/courses/mavl/mavl-thumbnail_ker6a6.svg"} />
+                <meta property="og:description" content={description} />
             </Head>
             <MainHeader />
             {children}
