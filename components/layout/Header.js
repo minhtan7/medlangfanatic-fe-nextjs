eb99031e3ad814fe2e4df9ae732fd0d94be47c3e
Tan Vo <tan.vopm@gmail.com>
2023-03-12 00:53:53 +0000

    modify heading ads

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -155,7 +155,7 @@ function MainHeader() {
                     <p className='m-0'>Học hỏi không giới hạn kiến thức chuyên khoa Y <br />
                         Đăng ký ngay Khoá học Combo từ vựng hoàn chỉnh <br />
                         <Link href="/courses/combo-vocabulary" className='text-decoration-none text-main'>
-                            <b>Tiết kiệm tới  31%</b>
+                            <b style={{ fontSize: "17px" }}>Tiết kiệm tới  31%</b>
                         </Link>
                     </p>
                     :
